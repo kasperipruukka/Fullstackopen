afefc28ed3293791ee343ec18e2c824aafc573ee
Kasperi Pruukka <55744887+kasperipruukka@users.noreply.github.com>
2025-11-23 17:38:12 +0200

    osa1 - 1.2 Kurssitiedot
    Refaktoroitu myös 1.1. Kurssitiedot -tehtävää.

--- osa1/1.2 Kurssitiedot/src/App.jsx
+++ osa1/1.2 Kurssitiedot/src/App.jsx
@@ -32,16 +32,27 @@ const Content = (props) => {
 
 	return (
 		<>
-			<p>{parts[0]} {exercises[0]}</p>
-			<p>{parts[1]} {exercises[1]}</p>
-			<p>{parts[2]} {exercises[2]}</p>
+			<Part part={parts[0]} exercise={exercises[0]} />
+			<Part part={parts[1]} exercise={exercises[1]} />
+			<Part part={parts[2]} exercise={exercises[2]} />
+		</>
+	)
+}
+
+const Part = (props) => {
+	const part = props.part;
+	const exercise = props.exercise;
+
+	return (
+		<>
+			<p>{part} {exercise}</p>
 		</>
 	)
 }
 
 const Total = (props) => {
 	const numberOfExercises = props.numberOfExercises
-	
+
 	return (
 		<>
 			<p>Number of exercises {numberOfExercises}</p>
