4765f9d1012d7269942e535b134587d0fbb95db3
Kasperi Pruukka <55744887+kasperipruukka@users.noreply.github.com>
2025-12-14 21:41:01 +0200

    Advanced useState -harjoittelua
    Jatketaan seuraavaksi "Tilan päivitys tapahtuu asynkronisesti"-otsikon alta...

--- osa1/harjoittelua - Advanced useState/src/App.jsx
+++ osa1/harjoittelua - Advanced useState/src/App.jsx
@@ -1,50 +1,35 @@
-import { useState } from 'react'
+import { useState } from "react";
 
 const App = () => {
-  const [counter, setCounter] = useState(0)
+  const [left, setLeft] = useState(0)
+  const [right, setRight] = useState(0)
 
-  console.log('rendering with counter value', counter)
+  const [allClicks, setAll] = useState([])
 
-  const increaseByOne = () => {
 
-    console.log('increasing, value before', counter)
-    setCounter(counter + 1)
+  const handleLeftClick = () => {
+    setAll(allClicks.concat('L'))
+    setLeft(left + 1)
   }
 
-  const decreaseByOne = () => { 
 
-    console.log('decreasing, value before', counter)
-    setCounter(counter - 1)
-  }
-
-  const setToZero = () => {
-
-    console.log('resetting to zero, value before', counter)
-    setCounter(0)
+  const handleRightClick = () => {
+    setAll(allClicks.concat('R'))
+    setRight(right + 1)
   }
 
   return (
     <div>
-      <Display counter={counter} />
-      <Button onClick={increaseByOne} text="plus" />
-      <Button onClick={setToZero} text="zero" />
-      <Button onClick={decreaseByOne} text="minus" />
+      <div>
+        {left}
+        <button onClick={handleLeftClick}>left</button>
+        <button onClick={handleRightClick}>right</button>
+        {right}
+
+        <p>{allClicks.join(' ')}</p>
+      </div>
     </div>
   )
-} 
-
-const Display = ({counter}) => {
-  return (
-    <div>{counter}</div>
-  )
 }
 
-const Button = (props) => {
-  return (
-    <button onClick={props.onClick}>
-      {props.text}
-    </button>
-  )
-}
-
-export default App
+export default App;
